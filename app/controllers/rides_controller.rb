3baa50133b288940452c07f17bbb4e2761320759
Your Name <arssonist@yahoo.com>
2016-08-24 10:46:09 -0400

    3 rides made

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -5,5 +5,7 @@ class RidesController < ApplicationController
     def lorem_ride
     render text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
   end
-
+  def ipsum_ride
+    render text: "Ipsum Lorem motherfucker!!"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/about', to: 'rides#about'
-  get '/lorem',  to: "rides#lorem_ride"
+  get '/lorem',  to: 'rides#lorem_ride'
+  get '/ipsum', to: 'rides#ipsum_ride'
 end
