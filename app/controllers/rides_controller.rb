e82817742f2571ae60b95a4f90d7836384db8d34
Your Name <arssonist@yahoo.com>
2016-08-24 10:42:17 -0400

    second commit of railsApp

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -2,4 +2,8 @@ class RidesController < ApplicationController
   def about
     render text: "Welcome to Ipsum's Fair! Come for our rides, stay for our hotdogs"
   end
+    def lorem_ride
+    render text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/about', to: 'rides#about'
+  get '/lorem',  to: "rides#lorem_ride"
 end
