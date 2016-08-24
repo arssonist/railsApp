e5e1e8df297204a518541b09e613b958b1c54b6b
Your Name <arssonist@yahoo.com>
2016-08-24 10:31:30 -0400

    railsApp in class

---
+++ app/controllers/rides_controller.rb
@@ -0,0 +1,5 @@
+class RidesController < ApplicationController
+  def about
+    render text: "Welcome to Ipsum's Fair! Come for our rides, stay for our hotdogs"
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  get '/about', to: 'rides#about'
+end
